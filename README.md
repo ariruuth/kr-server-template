4e7aba2fa1b52202caf5646cf91eb8671406d687
Ari Ruuth <aruuth@ipswitch.com>
2017-03-11 12:39:32 -0500

    cleanup

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# kr-server-template
+# Server
+
+## Build
+```
+mvn package
+```
+
+## Run
+```
+java -jar target/server-0.0.1-SNAPSHOT.jar
+```
+
+## Test in localhost
+```
+http://localhost:8080/api/v1/app/now
+http://localhost:8080/api/v1/app/now?iam=Santa
+```
--- src/main/java/com/kr/server/controller/ApplicationController.java
+++ src/main/java/com/kr/server/controller/ApplicationController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.net.InetAddress;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created on 3/11/17.
@@ -18,9 +20,14 @@ import java.util.Date;
 public class ApplicationController {
 
     @RequestMapping(value="/now", method = RequestMethod.GET)
-    public ResponseEntity<String> now(@RequestParam(value = "iam", required = false) String iam) {
-        String resp = (iam == null ? "" : "Hi " + iam + "! ") + "It is " + (new Date()) + " now over here. Br, " + getHost();
-        return new ResponseEntity<>(resp, HttpStatus.OK);
+    public ResponseEntity<Map> now(@RequestParam(value = "iam", required = false) String iam) {
+        Map response = new HashMap();
+        response.put("now", new Date());
+        response.put("My name is", getHost());
+        if (iam != null) {
+            response.put("Hi", iam);
+        }
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     private String getHost(){
